8e9a8341d74ba240484c9f1d4f2d344342695f1c
Christophere Johnson <christophere.johnson@YGS-MKTSVC45.local>
2023-11-12 17:18:44 -0500

    publishes bladewind comps

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -6,7 +6,27 @@
     <a href="{{route('accounts.create')}}">Track New Account</a>
 </div>
 <div>
-    <table>
+
+    <x-bladewind.table>
+        <x-slot name="header">
+            <th>Name</th>
+            <th>Department</th>
+            <th>Email</th>
+        </x-slot>
+        <tr>
+            <td>Alfred Rowe</td>
+            <td>Outsourcing</td>
+            <td>alfred@therowe.com</td>
+        </tr>
+        <tr>
+            <td>Michael K. Ocansey</td>
+            <td>Tech</td>
+            <td>kabutey@gmail.com</td>
+        </tr>
+    </x-bladewind.table>
+
+
+    {{-- <table>
         <thead>
             <tr>
                 <th>Player Tag</th>
@@ -36,7 +56,7 @@
     </table>
 
 
-    {{$accounts->links()}}
+    {{$accounts->links()}} --}}
 </div>
 
 @endsection
